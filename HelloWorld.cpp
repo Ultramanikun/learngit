87e9dff1420299022630345f4c2d9a55b3e3a0a4
wuminghao <2167884851@qq.com>
2025-11-17 19:37:04 +0800

    other HelloWorld.cpp revision

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,5 +4,6 @@ int main(){
 	cout<<"Hello World"<<endl;
 	cout<<"the first revision"<<endl;
 	cout<<"the second revision"<<endl;
-	cout<<"the branch revision"<<endl; 
+	cout<<"the branch revision"<<endl;
+	cout<<"next test revision"<<endl;
 }
